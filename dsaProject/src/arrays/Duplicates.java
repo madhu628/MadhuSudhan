907953cb5b599cf3ba4fe6601bab74a96b35bb2e
Lenovo <Lenovo@DESKTOP-OS7UM63>
2024-05-25 16:27:38 +0530

    duplicates

--- dsaProject/src/arrays/Duplicates.java
+++ dsaProject/src/arrays/Duplicates.java
@@ -1,14 +1,26 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class Duplicates {
+//	public static void main(String[] args) {
+////		int a[] = { 1, 4, 8, 2, 6, 8, 4 };
+////		Arrays.sort(a);
+////		int n = a.length;
+//////		for (int i = 0; i < n; i++) {
+//////			for (int j = i + 1; j < n; j++) {
+//////				if (a[i] == a[j]) {
+//////					System.out.println(a[i]);
+//////				}
+//////			}
+//////		}
 	public static void main(String[] args) {
 		int a[] = { 1, 4, 8, 2, 6, 8, 4 };
+		Arrays.sort(a);
 		int n = a.length;
-		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				if (a[i] == a[j]) {
-					System.out.println(a[i]);
-				}
+		for (int i = 1; i < n; i++) {
+			if (a[i] == a[i - 1]) {
+				System.out.println(a[i]);
 			}
 		}
 	}
