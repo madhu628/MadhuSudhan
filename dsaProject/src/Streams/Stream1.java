76c845c663ddd371fdbb9e84c0f267ac4f60eaa1
Lenovo <Lenovo@DESKTOP-OS7UM63>
2024-05-27 07:07:46 +0530

    update

--- dsaProject/src/Streams/Stream1.java
+++ dsaProject/src/Streams/Stream1.java
@@ -1,7 +1,8 @@
 package Streams;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -9,12 +10,15 @@ import java.util.stream.Collectors;
 public class Stream1 {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(1, 2, 3, 3, 4, 4, 5,6,1,7,9,0,2);
-		System.out.println(listDuplicateUsingFilterAndSetAdd(list));
+		System.out.println(listDuplicateUsingCollectionsFrequency(list));
 	}
 
-	static List<Integer> listDuplicateUsingFilterAndSetAdd(List<Integer> list) {
-		Set<Integer> elements = new HashSet<Integer>();
-		return list.stream().filter(n -> !elements.add(n)).collect(Collectors.toList());
+	static List<Integer> listDuplicateUsingCollectionsFrequency(List<Integer> list) {
+	    List<Integer> duplicates = new ArrayList<>();
+	    Set<Integer> set = list.stream()
+	      .filter(i -> Collections.frequency(list, i) > 1)
+	      .collect(Collectors.toSet());
+	    duplicates.addAll(set);
+	    return duplicates;
 	}
-
 }
